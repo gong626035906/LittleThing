dbae28ff959d9d4c52de5def8a6f9a01d4316615
gong626035906 <33952709+gong626035906@users.noreply.github.com>
2018-11-21 09:58:27 +0800

    Add files via upload

---
+++ RabbitMQ/ProjectReceive/Program.cs
@@ -0,0 +1,76 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjectReceive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var factory = new ConnectionFactory();
+            //factory.HostName = "localhost";
+            //factory.UserName = "wumerp";
+            //factory.Password = "wuerp";
+
+            //using (var connection = factory.CreateConnection())
+            //{
+            //    using (var channel = connection.CreateModel())
+            //    {
+            //        channel.QueueDeclare("hello", false, false, false, null);
+
+            //        var consumer = new EventingBasicConsumer(channel);
+            //        channel.BasicConsume("hello", false, consumer);
+            //        consumer.Received += (model, ea) =>
+            //        {
+            //            var body = ea.Body;
+            //            var message = Encoding.UTF8.GetString(body);
+            //            Console.WriteLine("已接收： {0}", message);
+            //            channel.BasicAck(ea.DeliveryTag, false);
+            //        };
+            //        Console.ReadLine();
+            //    }
+            //}
+
+            var factory = new ConnectionFactory();
+            factory.HostName = "192.168.22.76";
+            factory.UserName = "wumerp";
+            factory.Password = "wuerp";
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //channel.QueueDeclare("hello", false, false, false, null);
+                    channel.QueueDeclare("task_queue", true, false, false, null);
+                    channel.BasicQos(0, 1, false);
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    //channel.BasicConsume("hello", true, consumer);
+                    channel.BasicConsume("task_queue", false, consumer);
+
+                    while (true)
+                    {
+                        var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine("Received {0}", message);
+                        Console.WriteLine("Done");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ/RabbitMQ/Program.cs
@@ -0,0 +1,62 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var factory = new ConnectionFactory();
+            //factory.HostName = "localhost";//RabbitMQ服务在本地运行
+            //factory.UserName = "wumerp";//用户名
+            //factory.Password = "wuerp";//密码
+
+            //using (var connection = factory.CreateConnection())
+            //{
+            //    using (var channel = connection.CreateModel())
+            //    {
+            //        channel.QueueDeclare("hello", false, false, false, null);//创建一个名称为hello的消息队列
+            //        string message = "Hello World"; //传递的消息内容
+            //        var body = Encoding.UTF8.GetBytes(message);
+            //        channel.BasicPublish("", "hello", null, body); //开始传递
+            //        Console.WriteLine("已发送： {0}", message);
+            //        Console.ReadLine();
+            //    }
+            //}
+
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.UserName = "wumerp";
+            factory.Password = "wuerp";
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //channel.QueueDeclare("hello", false, false, false, null);
+                    channel.QueueDeclare("task_queue", true, false, false, null);
+                    string message = GetMessage(args);
+                    var properties = channel.CreateBasicProperties();
+                    properties.DeliveryMode = 2;
+                    properties.SetPersistent(true);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", "task_queue", properties, body);
+                    //channel.BasicPublish("", "hello", properties, body);
+                    Console.WriteLine(" set {0}", message);
+                }
+            }
+
+            Console.ReadKey();
+        }
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
